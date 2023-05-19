b14d1244afba1c39ce24f5e89e87b33637de15dd
ddsal07 <danidiaz0799@gmail.com>
2023-05-19 01:20:25 -0500

    Agregar archivos de proyecto.

---
+++ HospitalMVC/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using HospitalMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AuthenticationDbContext _context;
+
+        public UserController(AuthenticationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> UserIndex()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return View(users);
+        }
+
+    }
+}
---
+++ HospitalMVC/Models/AuthenticationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalMVC.Models;
+
+public partial class AuthenticationDbContext : DbContext
+{
+    public AuthenticationDbContext()
+    {
+    }
+
+    public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("users");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
